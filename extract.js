147965dfc385377cdfca8ad786e106c409855e89
HM <hieumdd@gmail.com>
2021-09-10 21:51:16 +0700

    feat: Added extract function

---
+++ extract.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const fs = require('fs/promises');
+const { BigQuery } = require('@google-cloud/bigquery');
+const { Storage } = require('@google-cloud/storage');
+
+const BQ_CLIENT = new BigQuery();
+const STORAGE_CLIENT = new Storage();
+
+const DATASET = 'NetSuite';
+const BUCKET = 'vnbi-temp-bucket';
+
+const extract = async () => {
+  const { tables } = JSON.parse(await fs.readFile('tables.json'));
+  const options = {
+    location: 'US',
+    format: 'csv',
+  };
+  await Promise.all(
+    tables.map(async (table) => {
+      const filename = `NetSuite_dump/${table}/${table}-*.csv`;
+      return BQ_CLIENT.dataset(DATASET)
+        .table(table)
+        .extract(STORAGE_CLIENT.bucket(BUCKET).file(filename), options);
+    }),
+  );
+};
+
+extract();
