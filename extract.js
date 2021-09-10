55f365220a8915cf616c66419553645559990b36
HM <hieumdd@gmail.com>
2021-09-10 22:19:28 +0700

    feat: Added npm scripts for extract & download

--- extract.js
+++ extract.js
@@ -8,6 +8,7 @@ const STORAGE_CLIENT = new Storage();
 
 const DATASET = 'NetSuite';
 const BUCKET = 'vnbi-temp-bucket';
+const FOLDER = 'NetSuite_dump';
 
 const extract = async () => {
   const { tables } = JSON.parse(await fs.readFile('tables.json'));
@@ -17,7 +18,7 @@ const extract = async () => {
   };
   await Promise.all(
     tables.map(async (table) => {
-      const filename = `NetSuite_dump/${table}/${table}-*.csv`;
+      const filename = `${FOLDER}/${table}/${table}-*.csv`;
       return BQ_CLIENT.dataset(DATASET)
         .table(table)
         .extract(STORAGE_CLIENT.bucket(BUCKET).file(filename), options);
